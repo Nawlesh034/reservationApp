24891154afeea510995acb4eca87614ab53bdcf5
Nawlesh034 <nn802301@gmail.com>
2025-01-03 16:04:59 +0530

    axios

--- src/axiosInstances.jsx
+++ src/axiosInstances.jsx
@@ -2,24 +2,12 @@ import axios from 'axios';
 
 // Create an Axios instance with base URL
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, // Replace with your backend URL if different
+  baseURL: "https://reservation-appbackend.vercel.app", // Replace with your backend URL if different
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Optional: Add request and response interceptors if needed
-// axiosInstance.interceptors.request.use((config) => {
-//   // Add any token or modify the request headers if necessary
-//   return config;
-// });
 
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     // Handle errors globally
-//     return Promise.reject(error);
-//   }
-// );
 
 export default axiosInstance;
